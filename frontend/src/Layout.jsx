dcac227048e769ac2a3212f083b99d74761e429a
Adnan Athania <adnanathania9@gmail.com>
2024-03-14 10:32:59 +0530

    newadded

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -8,17 +8,22 @@ const Layout = () => {
       <DashboardHeader />
       <div className="w-full flex h-[88%]">
         <Sidebar />
-        <div className="w-[85%] h-full overflow-y-scroll px-16 py-6 no-scrollbar">
+        <div className="w-[85%] z-50 h-full overflow-y-scroll px-16 py-6 no-scrollbar">
           <Outlet />
         </div>
       </div>
-      <img src={cube} className="cubeContainer absolute top-[43%] left-5" />
-      <img src={cube} className="cubeContainer absolute top-[25%] -right-32" />
       <img
         src={cube}
-        className="cubeContainer absolute bottom-[-35%] right-[40%]"
+        className="cubeContainer absolute top-[43%] z-10 left-5"
       />
-      
+      <img
+        src={cube}
+        className="cubeContainer absolute top-[25%] z-10 -right-32"
+      />
+      <img
+        src={cube}
+        className="cubeContainer absolute bottom-[-35%] z-10 right-[40%]"
+      />
     </div>
   );
 };
