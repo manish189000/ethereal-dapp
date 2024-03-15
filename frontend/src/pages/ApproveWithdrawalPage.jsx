6fdc7ae8e2df692c14406eb91a32e641c574b22a
Muzammil Malik <abdmalik1593@gmail.com>
2024-03-15 10:39:22 +0530

    changes

--- frontend/src/pages/ApproveWithdrawalPage.jsx
+++ frontend/src/pages/ApproveWithdrawalPage.jsx
@@ -1,5 +1,17 @@
 const ApproveWithdrawalPage = () => {
-  return <div>ApproveWithdrawalPage</div>;
+  return (
+    <div className="flex justify-between">
+      <div className="px-6 py-3 text-lw border border-[#141414] rounded-md w-[30%] flex justify-center bg-black shadow-lg">
+        <p className="text-base font-lw">Account</p>
+      </div>
+      <div className="px-6 py-3 text-lw border border-[#141414] rounded-md w-[30%] flex justify-center bg-black shadow-lg">
+        <p className="text-base font-lw">Amount</p>
+      </div>
+      <div className="px-6 py-3 text-lw border border-[#141414] rounded-md w-[30%] flex justify-center bg-black shadow-lg">
+        <p className="text-base font-lw">Approval</p>
+      </div>
+    </div>
+  );
 };
 
 export default ApproveWithdrawalPage;
